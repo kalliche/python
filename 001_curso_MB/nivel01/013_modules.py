fdcf727ba33ed84db19ddaee538e2c0f37f9e0dc
Carlos Garcia <kalliche16@gmail.com>
2023-06-09 05:55:32 -0500

    curso primer nivel terminado

--- 001_curso_MB/nivel01/013_modules.py
+++ 001_curso_MB/nivel01/013_modules.py
@@ -1,1 +1,14 @@
 # modules
+# para importar un modulo este debe estar renombrado sin mumeros formas nulas (010_variables) forma correcta variables
+
+import module
+module.sum_value(4,3,2)
+module.print_value('Carlos el mejor programador')
+
+from module import sum_value, print_value
+sum_value(5, 6, 7)
+print_value('Soy el mejor')
+
+import math
+print(math.pi)
+print(math.pow(2, 8))
---
+++ 001_curso_MB/nivel01/module.py
@@ -0,0 +1,5 @@
+def sum_value(number_one, number_two, number_three):
+    print(number_one + number_two + number_three)
+
+def print_value(value):
+    print(value)
