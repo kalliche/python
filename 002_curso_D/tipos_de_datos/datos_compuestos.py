98e1189f43d64e310b322646b566bcc55fc948dd
REINALGO NASTAR <crgarcian17m@itp.edu.co>
2023-06-15 17:03:20 -0500

    curso_D en marcha

---
+++ 002_curso_D/tipos_de_datos/datos_compuestos.py
@@ -0,0 +1,7 @@
+'''Listas se puede modificar'''
+lista = ['Carlos Garcia', 'Soy caliche', True, 1.65]
+print(lista)
+print(lista[0])
+
+'''Tuplas: no se puede modificar, solo iterar'''
+tupla = ('Carlos Garcia', 'Soy caliche', True, 1.65)
---
+++ 002_curso_D/variables/variables.py
@@ -0,0 +1,10 @@
+a = 15
+b = 5
+c = a + b
+print(c)
+
+'''las variables se declaran, las variables son modificadas, redifiniendo una vez cambien'''
+
+nombre = 'Carlos'
+bienvenido = f'Hola {nombre} ¿como estas?'
+print(bienvenido)
