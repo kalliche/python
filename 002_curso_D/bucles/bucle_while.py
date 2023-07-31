01e34a14668dd5b9b24ddf0c06655fed1f7ea777
REINALGO NASTAR <crgarcian17m@itp.edu.co>
2023-07-31 14:36:51 -0500

    ejercicios Dalto

---
+++ 002_curso_D/bucles/bucle_while.py
@@ -0,0 +1,8 @@
+# creando un contador que va a ir sumandose
+contador = 0
+
+# mientras que la condicion se cumpla el bucle se va a seguir ejecutando
+# (vuelta tras vuelta se verifica la condicion)
+while contador < 10:
+    print(contador)
+    contador += 1
---
+++ 002_curso_D/funciones/build_in.py
@@ -0,0 +1,13 @@
+numeros = [4,7,1,45,19]
+
+# encontrando el numero mas alto
+numero_mas_alto = max(numeros)
+print(numero_mas_alto)
+
+# encontrando el numero mas bajo
+numero_mas_bajo = min(numeros)
+print(numero_mas_bajo)
+
+# numero de decimales a imprimir
+numero = round(12.345678,3)
+print(numero)
