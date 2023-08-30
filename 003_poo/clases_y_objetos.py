86572b6a4da57e81f9153b332d2a75e4bf678fbb
Carlos Garcia Nastar <kalliche16@gmail.com>
2023-08-30 06:17:43 -0500

    metodos en poo

---
+++ 003_poo/clases_y_objetos.py
@@ -0,0 +1,31 @@
+celular1_marca = 'samsung'
+celular2_marca = 'apple'
+celular3_marca = 'huawei'
+
+celular1_modelo = 'S23'
+celular2_modelo = 'iphone 15 pro'
+celular3_modelo = 'p20 pro'
+
+celular1_camaraT = '48px'
+celular2_camaraT = '48px'
+celular3_camaraT = '12px'
+
+celular1_marcaF = '24px'
+celular2_marcaF = '24px'
+celular3_marcaF= '8px'
+
+print(celular2_marcaF)
+print('--' * 15)
+
+'''Forma corrwcta de como utilizar y optimizar codigo'''
+class Celular():
+    marca = 'samsung'
+    modelo = 's23'
+    camara = '40px'
+
+celular01 = Celular()
+print(celular01.marca)
+print(celular01.modelo)
+print(celular01.camara)
+
+print('--' * 15)
